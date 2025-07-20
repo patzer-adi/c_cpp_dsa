6ba052f56087a51cd6d082a6a8d01df31f59e338
patzer-adi <nkadityagowari@gmail.com>
2025-07-20 17:31:16 +0530

    swap with using temporary var

---
+++ swap_with.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int n1, n2,temp;
+    printf("Enter two numbers");
+    scanf("%d",&n1,&n2);
+    printf("Before swap:\nn1: %d\nn2: %d\n",n1,n2);
+    temp = n1;
+    n1 = n2;
+    n2 = temp;
+    printf("After swap:\nn1: %d\nn2: %d\n",n1,n2);
+    return 0;
+}
