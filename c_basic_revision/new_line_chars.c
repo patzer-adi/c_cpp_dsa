f1258daede4c343bbbf4b03ec18bb1dc1748b28e
patzer-adi <nkadityagowari@gmail.com>
2025-07-27 19:54:00 +0530

    Escape sequence types

---
+++ c_basic_revision/new_line_chars.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Escape Sequences:");
+    printf("\nthis is new line \n");
+    printf("\nthis is tab \t");
+    printf("\nthis is vertical \v");
+    printf("\nthis is  backspace \b");
+    printf("\nthis is carriage \r");
+    printf("\nthis is forrfed \f");
+    printf("\nthis is \'single quote\' \n");
+    printf("\nthis is \? i dont know \n");
+
+
+
+}
