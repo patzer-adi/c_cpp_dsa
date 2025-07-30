76a5addc582cba649a6c6f1d14f678d1ec2b2dcc
AdityaGowari22 <nkadityagowari@gmail.com>
2025-07-30 11:22:19 +0530

    Sum of square: 1^2+ 1^2+ 1^3.... 1^n

---
+++ c_basic_revision/sum_squares.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int n,ans=0;
+    printf("Enter a number:\n");
+    scanf("%d",&n);
+
+    for(int i = 0; i < n; ++i)
+    {
+        ans = ans + n*n;
+        printf("%d\n",ans);
+
+    }
+    printf("Answer: %d",ans);
+    return 0;
+}
