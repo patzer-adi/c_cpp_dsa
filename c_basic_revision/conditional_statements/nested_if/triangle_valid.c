08fb21641d542d00ee3b24204bde206c68ccd61b
AdityaGowari22 <nkadityagowari@gmail.com>
2025-07-31 17:27:06 +0530

    Triangle valid through angles

---
+++ c_basic_revision/conditional_statements/nested_if/triangle_valid.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int a1,a2,a3;
+    printf("Enter three angles of a triangle:\n");
+    scanf("%d %d %d",&a1,&a2,&a3);
+    
+    if( a1 + a2 + a3 == 180)
+        printf("Triangle is valid\n");
+    else
+        printf("It is not a triangle mathematically");
+
+    return 0;
+}
