92a13ce689eaab11722c0cf364ac625faa05f8e8
AdityaGowari22 <nkadityagowari@gmail.com>
2025-08-05 11:27:31 +0530

    Power intresting short code

---
+++ c_basic_revision/loops/while/pow_short.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int x,y,count=0,pow=1;
+    printf("Enter a number x:\n");
+    scanf("%d",&x);
+    printf("Enter a number y:\n");
+    scanf("%d",&y);
+    while(y--)
+    {
+       pow = pow*x;
+       printf("pow: %d\n",pow);
+    }
+    printf("Power: %d\n",pow);
+    return 0;
+}
