d20fee128785f2232780cd02c41deb873f867cfd
AdityaGowari22 <nkadityagowari@gmail.com>
2025-08-07 22:36:43 +0530

    new type of conditions

---
+++ c_basic_revision/loops/for/for_loop_try/prime_num_cond.c
@@ -0,0 +1,25 @@
+// check this as well 
+
+#include<stdio.h>
+
+int main()
+{
+    int n,i=1,cnt=0;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    printf("factors:\n");
+    for(i= 1;i <= n ; i++)
+    {
+        if(n % i == 0)
+        {
+            printf("%i,\t",i);
+            break;
+        }
+    }
+
+    if(cnt == 0)
+        printf("%d is a prime\n",n);
+    else
+        printf("%d is not a prime\n",n);
+    return 0;
+}
