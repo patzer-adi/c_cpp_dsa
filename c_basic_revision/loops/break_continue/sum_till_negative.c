480023d2303f9ee8a5b0389d822f9b2c672b069d
AdityaGowari22 <nkadityagowari@gmail.com>
2025-08-12 10:19:03 +0530

    Sum till -ve entered --updated

--- c_basic_revision/loops/break_continue/sum_till_negative.c
+++ c_basic_revision/loops/break_continue/sum_till_negative.c
@@ -3,16 +3,20 @@
 void run()
 {
     int n;
+    int sum = 0;
     while(1)
     {
     printf("Enter numbers:\n");
     scanf("%d",&n);
     if(n<0)
     {
-        printf("-ve number entered:");
+        printf("-ve number entered: (exit)\n");
         break;
     }
+    sum += n;
     }
+
+    printf("Sum of the +ve numbers is %d",sum);
 }
 
 int main()
