ea5f78c9db47b69001dbbd33308fb084ed96b01c
AdityaGowari22 <nkadityagowari@gmail.com>
2025-08-21 11:03:54 +0530

    Sanika Mahind q1

---
+++ Test/Test_1/every_other_ques/sanikaM_q1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// same as varad's question
+
+void run()
+{
+    int n;
+    printf("Enter score (0-100):\n");
+    scanf("%d", &n);
+
+    (n >= 0 && n <= 100) ? 
+        (n >= 40 ? 
+            (printf("Pass\n"), (n >= 90 ? printf("Excellent!\n") : printf("Keep improving!\n"))) 
+            : printf("Fail\n")) 
+        : printf("Invalid Input\n");
+}
+
+int main()
+{
+    run();
+    return 0;
+}
