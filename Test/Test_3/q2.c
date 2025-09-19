8d415837ecb0bc4a331ddc653a0b3f83b109d778
AdityaGowari22 <nkadityagowari@gmail.com>
2025-09-19 09:17:18 +0530

    question 2

---
+++ Test/Test_3/q2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void run()
+{
+    int n, i = 1;
+
+    // Accept input from the user
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    // Outer loop for each line
+    while (i <= n) {
+        int j = 1;
+
+        // Inner loop to print numbers from 1 to i
+        while (j <= i) {
+            printf("%d ", j);
+            j++;
+        }
+
+        printf("\n");  // Move to next line
+        i++;
+    }
+    return;
+}
+
+int main() {
+    run();
+    return 0;
+}
