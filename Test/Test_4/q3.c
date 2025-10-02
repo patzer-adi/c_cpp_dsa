8cb0ff47d4d9d7a450100183778eb280e23a14e3
AdityaGowari22 <nkadityagowari@gmail.com>
2025-10-02 19:04:51 +0530

    this is q3 of test

---
+++ Test/Test_4/q3.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void sortArray(int *arr, int n) {
+    int i, j, temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n, i;
+    int *arr;
+
+    printf("Enter the number of integers: ");
+    scanf("%d", &n);
+
+    arr = (int *)malloc(n * sizeof(int));
+    if (arr == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    printf("Enter %d integers:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (i = 0; i < n; i++) {
+        if (arr[i] % 2 == 0) {
+            arr[i] = 0; // Even
+        } else {
+            arr[i] = 1; // Odd
+        }
+    }
+
+    sortArray(arr, n);
+
+    printf("Output: [");
+    for (i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i < n - 1) {
+            printf(", ");
+        }
+    }
+    printf("]\n");
+
+    free(arr);
+    return 0;
+}
