a3020b0b194e2c87b831b4e138ce0ed9b55c3986
AdityaGowari22 <nkadityagowari@gmail.com>
2025-11-02 18:13:09 +0530

    pattern_1

---
+++ c_basic_revision/loops/patterns/patterns_6_by_sir/pattern_1.c
@@ -0,0 +1,26 @@
+// 1
+// 1       2
+// 1       2       3
+// 1       2       3       4
+#include<stdio.h>
+
+void run()
+{
+    int n;
+    printf("Enter n:\n");
+    scanf("%d",&n);
+    for(int i = 1; i <= n; i ++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            printf("%d\t",j);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    run();
+    return 0;
+}
