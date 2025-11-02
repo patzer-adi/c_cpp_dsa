8660ba80f0750d1e591674a7366d20edb93bbd39
AdityaGowari22 <nkadityagowari@gmail.com>
2025-11-02 18:13:31 +0530

    this is pattern _ 2

---
+++ c_basic_revision/loops/patterns/patterns_6_by_sir/pattern_2.c
@@ -0,0 +1,28 @@
+//                         1
+//                 1       2
+//         1       2       3
+// 1       2       3       4
+#include<stdio.h>
+
+void run()
+{
+    int n;
+    printf("Enter n:\n");
+    scanf("%d",&n);
+    for(int i = 1; i <= n; i ++)
+    {
+        for(int j = i; j < n; j++)
+            printf("\t");
+        for(int j = 1; j <= i; j++)
+        {
+            printf("%d\t",j);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    run();
+    return 0;
+}
