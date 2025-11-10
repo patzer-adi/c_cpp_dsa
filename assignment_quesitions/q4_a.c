6225453f5d10ac78053816ee1e4ac736ee219608
AdityaGowari22 <nkadityagowari@gmail.com>
2025-11-10 10:33:57 +0530

    just push

---
+++ assignment_quesitions/q4_a.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int n1,n2;
+    printf("Enter n1 and n2:\n");
+    scanf("%d %d",&n1,&n2);
+    printf("\nAddition: %d",n1+n2);
+    printf("\nSubstraction: %d",n1-n2);
+    printf("\nMultiplication: %d",n1*n2);
+    printf("\nDivision: %d",n1/n2);
+    printf("\nRemainder: %d",n1%n2);
+
+    return 0;
+}
