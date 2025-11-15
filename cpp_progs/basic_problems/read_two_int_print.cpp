cdfd79fcdfa1037199c2adbc3aabfb5bfeeeae61
AdityaGowari22 <nkadityagowari@gmail.com>
2025-11-15 08:25:55 +0530

    read two integeres and print them

---
+++ cpp_progs/basic_problems/read_two_int_print.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+    cout << "You entered: " << a << " and " << b;
+    return 0;
+}
