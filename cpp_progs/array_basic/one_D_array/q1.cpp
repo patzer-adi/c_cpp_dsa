1874b9ab139077b8ac52445851b5d63f5a862177
AdityaGowari22 <nkadityagowari@gmail.com>
2025-12-02 08:22:05 +0530

    without array

---
+++ cpp_progs/array_basic/one_D_array/q1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d, e;
+    cout << "Enter five integers: ";
+    cin >> a >> b >> c >> d >> e;
+
+    cout << "You entered: ";
+    cout << a << " " << b << " " << c << " " << d << " " << e;
+
+    return 0;
+}
