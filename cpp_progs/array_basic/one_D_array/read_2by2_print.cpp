fff5ce131065372bbbdcf0d8e12da9cbf652aa94
AdityaGowari22 <nkadityagowari@gmail.com>
2025-12-05 08:24:35 +0530

    read a 2 by 2 and print

---
+++ cpp_progs/array_basic/one_D_array/read_2by2_print.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[2][2];
+
+    cout << "Enter a 2x2 matrix:\n";
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            cin >> a[i][j];
+        }
+    }
+
+    cout << "Matrix:\n";
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++){
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
