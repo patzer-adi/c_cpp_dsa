5543707d3985df79f68814e2c1aee940273f5e76
AdityaGowari22 <nkadityagowari@gmail.com>
2025-12-07 18:04:16 +0530

    read character in array

---
+++ cpp_progs/array_basic/one_D_array/read_char.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch;
+
+    cout << "Enter a character: ";
+    cin >> ch;
+
+    cout << "You entered: " << ch;
+    return 0;
+}
