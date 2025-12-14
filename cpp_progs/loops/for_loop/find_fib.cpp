0aa07402e51e99097c8086b165cee5c8d62db5c4
AdityaGowari22 <nkadityagowari@gmail.com>
2025-12-14 10:58:30 +0530

    find fibonacci series '

---
+++ cpp_progs/loops/for_loop/find_fib.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    int a = 0, b = 1;
+
+    cout << a << " " << b << " ";
+
+    for (int i = 3; i <= n; i++) {
+        int c = a + b;
+        cout << c << " ";
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
