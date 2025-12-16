9d17ac29f5004d307b04d082e70ed5192eb2efb1
AdityaGowari22 <nkadityagowari@gmail.com>
2025-12-16 09:55:32 +0530

    numbers digits in an integer

---
+++ cpp_progs/loops/for_loop/number_digits_in_int.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, count = 0;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n == 0) count = 1;
+
+    while (n != 0) {
+        count++;
+        n /= 10;
+    }
+
+    cout << "Number of digits = " << count;
+    return 0;
+}
