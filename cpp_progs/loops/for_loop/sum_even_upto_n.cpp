11ab8eeac1fee607b97422dd92ad12002bcd51a5
AdityaGowari22 <nkadityagowari@gmail.com>
2025-12-21 00:26:01 +0530

    sum even numbers upto n

---
+++ cpp_progs/loops/for_loop/sum_even_upto_n.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N, sum = 0;
+    cout << "Enter N: ";
+    cin >> N;
+
+    for (int i = 2; i <= N; i += 2) {
+        sum += i;
+    }
+
+    cout << "Sum of even numbers: " << sum;
+    return 0;
+}
