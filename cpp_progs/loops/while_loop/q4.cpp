c4642d382913fcde55a8212f35cc39abd79d9703
AdityaGowari22 <nkadityagowari@gmail.com>
2025-12-24 09:34:24 +0530

    q4.cpp

---
+++ cpp_progs/loops/while_loop/q4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, i = 2, sum = 0;
+    cin >> n;
+    while (i <= n) {
+        sum += i;
+        i += 2;
+    }
+    cout << sum;
+}
