03f06bdfe44bcd733b2971e6aa47de954a6cd7f4
AdityaGowari22 <nkadityagowari@gmail.com>
2025-12-29 14:46:59 +0530

    q19.cpp

---
+++ cpp_progs/loops/while_loop/q19.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, i = 1;
+    cin >> n;
+    while (i <= 10) {
+        cout << n << " x " << i << " = " << n*i << endl;
+        i++;
+    }
+}
