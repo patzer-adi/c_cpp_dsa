fd4fabed250f6d50882b7ef406f2fd1bb2e33141
AdityaGowari22 <nkadityagowari@gmail.com>
2026-01-03 09:38:30 +0530

    q6.cpp

---
+++ cpp_progs/loops/do_while/q6.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n,sum=0;
+    scanf("%d",&n);
+    do {
+        sum += n % 10;
+        n /= 10;
+    } while(n != 0);
+    printf("%d",sum);
+    return 0;
+}
