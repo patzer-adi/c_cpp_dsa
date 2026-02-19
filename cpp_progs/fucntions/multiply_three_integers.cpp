ca51c4c7cfebe413ffef9b1d4c22abadcc70ab5b
AdityaGowari22 <nkadityagowari@gmail.com>
2026-02-19 08:53:00 +0530

    three integers

---
+++ cpp_progs/fucntions/multiply_three_integers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int multiply(int a, int b, int c) {
+    return a * b * c;
+}
+
+int main() {
+    cout << "Product = " << multiply(2, 3, 4);
+    return 0;
+}
